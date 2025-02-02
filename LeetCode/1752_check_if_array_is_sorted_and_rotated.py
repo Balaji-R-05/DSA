9a6458bde1c75a5745df5bcb8d2a322caeb7d72c
Balaji R <balajiramu23@gmail.com>
2025-02-02 11:37:45 +0530

    Modified README.md and added 1752

---
+++ LeetCode/1752_check_if_array_is_sorted_and_rotated.py
@@ -0,0 +1,16 @@
+class Solution:
+    def check(self, nums: list[int]) -> bool:
+        nums_length = len(nums)
+        count = 0
+        for i in range(0,nums_length-1):
+            if nums[i]>nums[i+1]:
+                count+=1
+        if nums[0]<nums[nums_length-1]:
+            count+=1
+        return count<=1
+    
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+Solution().check([3,4,5,1,2]) # True
+Solution().check([2,1,3,4]) # False
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # DSA
 
+## LeetCode
 - **1. Two Sum**
 - **7. Reverse Integer**
 - **3151. Special Array 1**
+
+## GeeksforGeeks
