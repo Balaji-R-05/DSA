7278702b6a5d9e4f3fe5a676e29cbe1c4de9982f
Balaji R <balajiramu23@gmail.com>
2025-02-06 11:10:27 +0530

    Added 42 in Python and Java

---
+++ LeetCode/Solution_42.java
@@ -0,0 +1,31 @@
+// 42. Trapping Rain Water
+
+class Solution_42 {
+    public int trap(int[] height) {
+        int vol = 0;
+        int i = 0, j = height.length - 1;
+        int lmax = height[i], rmax = height[j];  // Initialize lmax and rmax correctly
+        
+        while (i < j) {
+            if(height[i] <= height[j]) {
+                if (height[i] < lmax) {
+                    vol += lmax - height[i];
+                }else {
+                    lmax = height[i];
+                }
+                i++;
+            }else{
+                if(height[j] < rmax) {
+                    vol += rmax - height[j];
+                }else{
+                    rmax = height[j];
+                }
+                j--;
+            }
+        }  
+        return vol;
+    }
+}
+
+// Time complexity: O(n)
+// Space complexity: O(1)
