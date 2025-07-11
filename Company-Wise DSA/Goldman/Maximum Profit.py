72f91c261a2042276fa36755c424cc30da29cf57
Balaji-R-05 <balajiramu23@gmail.com>
2025-07-11 23:40:14 +0530

    Started Company wise DSA

---
+++ Company-Wise DSA/Goldman/Maximum Profit.py
@@ -0,0 +1,12 @@
+# Maximum Profit
+
+# You have N people standing in a queue. Each person in the queue has either made a profit or incurred a loss, represented by an array nums[], where nums[i] indicates the profit or loss of the ith person:
+# If nums[i] is positive, the person has made a profit.
+# If nums[i] is negative, the person has incurred a loss.
+# Your task is to determine the maximum net total profit that can be obtained from a group of people standing consecutively in the queue.
+
+# Input:
+# 7
+# -1 -2 3 4 -1 2 1
+# Output:
+# 9
