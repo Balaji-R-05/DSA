032dd647df33d37399b43be06a1feeeb29d77898
Balaji-R-05 <balajiramu23@gmail.com>
2026-02-19 11:00:00 +0530

    Added ABC-43 D

--- AtCoder/ABC_043_D.java
+++ AtCoder/ABC_043_D.java
@@ -5,16 +5,17 @@ public class ABC_043_D {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
         int n = s.length();
-
         for (int i = 0; i + 1 < n; i++) {
             if (s.charAt(i) == s.charAt(i + 1)) {
                 System.out.println((i + 1) + " " + (i + 2));
+                sc.close();
                 return;
             }
         }
         for (int i = 0; i + 2 < n; i++) {
             if (s.charAt(i) == s.charAt(i + 2)) {
                 System.out.println((i + 1) + " " + (i + 3));
+                sc.close();
                 return;
             }
         }
---
+++ AtCoder/README.md
@@ -0,0 +1,9 @@
+# AtCoder
+
+This folder contains the solutions to the problems on AtCoder.
+
+## Completed Contests
+
+- ABC 042 - A,B
+- ABC 043 - All
+- ABC 044 - A,B
