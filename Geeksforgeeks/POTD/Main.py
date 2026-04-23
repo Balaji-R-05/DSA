06a80b6910b8cc9db60145901b19e74b4626c83a
Balaji-R-05 <balajiramu23@gmail.com>
2026-04-24 00:45:42 +0530

    Added GFG POTD 24-04-2026

---
+++ Geeksforgeeks/POTD/Main.py
@@ -0,0 +1,16 @@
+# Buildings with Sunlight
+
+# Given an array of positive integers representing the heights of buildings, determine how many buildings will receive sunlight. A building receives sunlight if there are no taller buildings to its right.
+
+class Solution:
+    def visibleBuildings(self, arr):
+        res = 1
+        curr = arr[0]
+        for i in range(1, len(arr)):
+            if arr[i] >=  curr:
+                res += 1
+                curr = arr[i]
+        return res
+    
+# Time Complexity: O(N)
+# Space Complexity: O(1)
---
+++ Geeksforgeeks/POTD/README.md
@@ -0,0 +1,3 @@
+# Geeksforgeeks POTD
+
+- **24-04-2026** - Buildings with Sunlight
